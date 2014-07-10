4d62c5713d43641c417282d2a93159def2cb8d18
jaunvc123 <soporte@asopagos.co>
2014-07-10 11:50:49 -0500

    .

--- ejemplos/backend/script.js
+++ ejemplos/backend/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 	
+
 	var button = document.getElementById("buttonAtras")
 	var iframe = document.getElementById("todoIframe")
 	var dir = 'http://todo-super.herokuapp.com/'
@@ -8,6 +9,16 @@ document.addEventListener('DOMContentLoaded', function(){
 	button.addEventListener('click', function(e) {
 		console.log(iframe)
 		iframe.setAttribute('src', dir)
+	})
+
+	$('#iframeChat').on('load', function() {
+		console.log('lol')
+		$('.waitMessage').animate({
+				top: "-100px"
+			},
+			{
+				duration: 500				
+			})
 	})
 
 })
